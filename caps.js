92f401c81d2d09c6d0a23f402abfe7e742669967
Cody Carpenter <cody_carpenter2222@yahoo.com>
2021-04-03 21:29:52 -0700

    done

--- caps.js
+++ caps.js
@@ -1,34 +1,36 @@
 'use strict'
 
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
-const io = require('socket.io')(PORT);
+
+const port = process.env.PORT || 3000;
+
+const io = require('socket.io')(port);
+const caps = io.of('/caps');
 
 io.on('connection', (socket) => {
-  console.log(`Hello ${socket.id}`)
+  console.log('Welcome', socket.id);
+})
+
+caps.on('connection', (socket) => {
 
   socket.on('pickup', (payload) => {
-  socket.broadcast.emit('newPickup', payload);
+    socket.broadcast.emit('pickup', payload);
   })
+  console.log('connected to caps namespace:', socket.id)
 
   socket.on('in-transit', (payload) => {
-    setTimeout(() => {
-      socket.emit('sendMessage', payload)
-    }, 3000)
-  })
+    socket.emit('in-transit', payload);
+    console.log('heard in-transit')
+  });
 
   socket.on('delivered', (payload) => {
     socket.broadcast.emit('delivered', payload)
+    console.log('heard delivered');
   })
 
-})
+  socket.on('received', (payload) => {
+    console.log('message recieved')
+  })
+});
 
-  function sendMessage(payload) {
-    console.log('EVENT', { event: ``, time: `${faker.date.soon()}`, payload });
-  } 
-
-module.exports = {
-  sendMessage,
-}
-
-console.log('CAPS TURNED ON...');
+console.log('HUB CONNECTED');
